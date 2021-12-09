fb2f00b4e2819d94262a2cd4ab03f099ea0fa410
daniil raby <daniil.raby@tietoevry.com>
2021-12-09 18:23:13 +0300

    dev in progress

---
+++ ForceQuit/ForceQuit/MainScreenModule/MainScreenController.swift
@@ -0,0 +1,29 @@
+//
+//  MainScreenController.swift
+//  ForceQuit
+//
+//  Created by mac on 9.12.21.
+//
+
+import Cocoa
+
+final class MainScreenController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+    private func terminateAllApps() {
+        let openApps = NSWorkspace.shared.runningApplications
+        for app in openApps where app.activationPolicy == .regular {
+            app.forceTerminate()
+        }
+    }
+
+    // can get icons from NSRunningAplication, title - the same
+
+    @objc private func ternimateApp() {
+        NSApplication.shared.terminate(AnyObject.self)
+    }
+}
