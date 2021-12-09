1696e88093bf322bc5ad22a56dc3e874e33ebd14
daniil raby <daniil.raby@tietoevry.com>
2021-12-09 18:25:09 +0300

    for status bar popover

--- ForceQuit/ForceQuit/SupportingFiles/AppDelegate.swift
+++ ForceQuit/ForceQuit/SupportingFiles/AppDelegate.swift
@@ -28,3 +28,31 @@ class AppDelegate: NSObject,
         return true
     }
 }
+
+
+//func applicationDidFinishLaunching(_ aNotification: Notification) {
+//    let contentView = ContentView()
+//
+//    let popover = NSPopover()
+//    popover.contentSize = NSSize(width: contentView.width, height: contentView.height)
+//    popover.behavior = .transient
+//    popover.contentViewController = NSHostingController(rootView: contentView)
+//    self.popover = popover
+//
+//    statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+//
+//    if let button = statusBarItem.button {
+//        button.image = NSImage(systemSymbolName: "xmark.circle", accessibilityDescription: "Nuke")
+//        button.action = #selector(togglePopover(_:))
+//    }
+//}
+//
+//@objc func togglePopover(_ sender: AnyObject?) {
+//    if let button = statusBarItem.button {
+//        if popover.isShown {
+//            popover.performClose(sender)
+//        } else {
+//            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+//        }
+//    }
+//}
