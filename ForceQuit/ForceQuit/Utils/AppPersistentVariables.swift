ceb262f9c85431f63513dc78f7c1cd57a0437c17
daniil raby <daniil.raby@tietoevry.com>
2021-12-09 12:59:43 +0300

    initial commit

---
+++ ForceQuit/ForceQuit/Utils/AppPersistentVariables.swift
@@ -0,0 +1,14 @@
+//
+//  AppPersistentVariables.swift
+//  ForceQuit
+//
+//  Created by mac on 9.12.21.
+//
+
+import Foundation
+
+struct AppPersistentVariables {
+    public static var baseUrl: URL? {
+        return URL(string: "https://wellhello.com/site/user/home?chatOpened=1")
+    }
+}
